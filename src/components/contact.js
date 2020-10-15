a4b7f6e293c7899be0029eec8f5bdcd6eac0230d
Raphael Lefevre <raph.lefevre@gmail.com>
2020-10-15 08:58:47 +0200

    status update onClick

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,30 +1,73 @@
+import userEvent from "@testing-library/user-event";
 import React from "react";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import "./contact.css";
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img
-        className="avatar"
-        src={props.avatar}
-        alt={props.name}
-      />
-      <div>
-        <h4 className="name">{props.name}</h4>
-        <div className="status">
-          <span className={props.online ? "status-online" : "status-offline"}></span>
-          <p className="status-text">{props.online ? "online" : "offline"}</p>
+// function Contact({gender, avatar, name, online}) {
+//   return (
+//     <div className="Contact" style={gender==="female" ? {background:"pink"} : {background:"lightblue"}}>
+//       <img
+//         className="avatar"
+//         src={avatar ? avatar : "https://hr-geneve.ch/wp-content/uploads/2016/08/default-avatar.jpg"} 
+//         alt={name}
+//       />
+//       <div>
+//         <h4 className="name">{name}</h4>
+//         <div className="status">
+//           <span className={online ? "status-online" : "status-offline"}></span>
+//           <p className="status-text">{online ? "online" : "offline"}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// Contact.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     image: PropTypes.string.isRequired,
+//     online: PropTypes.bool,
+//   };
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false
+    }
+  }
+
+  
+  render() {
+    return (
+      <div className="Contact" style={this.props.gender==="female" ? {background:"pink"} : {background:"lightblue"}}>
+        <img
+          className="avatar"
+          src={this.props.avatar ? this.props.avatar : "https://hr-geneve.ch/wp-content/uploads/2016/08/default-avatar.jpg"} 
+          alt={this.props.name}
+        />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <div 
+            className="status"
+            onClick={event => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+              }}
+            >
+              <span className={this.state.online ? "status-online" : "status-offline"}></span>
+              <p className="status-text">{this.state.online ? "online" : "offline"}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    online: PropTypes.bool,
-  };
+
+
+
+
+
+
 
 export default Contact;
--- src/components/contactList.js
+++ src/components/contactList.js
@@ -6,34 +6,43 @@ const users = [
     {
       name: 'Robert Reyes',
       avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
+      isConnected: false,
+      gender: 'male',
     },
     {
       name: 'Nellie Caldwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
+
+      isConnected: true,
+      gender: 'female',
     },
     {
       name: 'Vernon Mason',
       avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
+      isConnected: true,
+      gender: 'male',
     },
     {
       name: 'Erica Hunt',
       avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
+      isConnected: false,
+      gender: 'female',
     },
     {
       name: 'Juanita Phillips',
       avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
+      isConnected: true,
+      gender: 'female',
     }
   ];
 
 const contacts = () => (
     <div>
-        {users.map(user => (Contact(user)
-        ))}
+        {users.map(user => (<Contact
+        name={user.name}
+        gender={user.gender}
+        avatar={user.avatar}
+        key={user.name}
+        />))}
     </div>
 
 );
